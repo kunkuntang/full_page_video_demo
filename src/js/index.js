6d5662422c552778b4a3334d3345540c05b7fd73
kuntang <kuntang@163.com>
2018-05-31 16:30:15 +0800

    fixed some bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,30 @@
 window.onload = function() {
   console.log('index had been loaded!')
 
+  var winH = window.innerHeight || document.body.clientHeight || document.body.offsetHeight,
+    winW = window.innerWidth || document.body.clientWidth || document.body.offsetWidth;
+
   function g(id) {
     return document.getElementById(id);
   }
-  var mainTitle, secTitle = null;
+  var mainTitle, secTitle, video, videoH, videoW = null;
   mainTitle = g('mainTitle');
   secTitle = g('secTitle');
+  video = g('video')
+  videoH = video.videoHeight;
+  videoW = video.videoWidth;
+
+  function resetVideoCon() {
+    if (winH / videoH < winW / videoW) {
+      video.style.width = winW + 'px';
+    } else {
+      video.style.height = winH + 'px';
+    }
+  }
+
+  resetVideoCon();
+
+  window.onresize = resetVideoCon;
 
   setTimeout(() => {
     mainTitle.style.marginTop = 0;
